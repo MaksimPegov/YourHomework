0bb7bd40bfd7265d14d68c5232fa28975eb00c62
Maksim Pegov <maksim.peg@gmail.com>
2022-11-21 23:33:06 +0100

    [#4] Email check on login page

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,12 +1,68 @@
 import { motion } from 'framer-motion'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { TextField, Box, Link, Button } from '@mui/material'
-import { MailOutline, VpnKey } from '@mui/icons-material'
+import React, { useEffect } from 'react'
+import {
+  TextField,
+  Box,
+  Link,
+  Button,
+  InputAdornment,
+  IconButton,
+  Input,
+  FormControl,
+  InputLabel,
+} from '@mui/material'
+import { MailOutline, VpnKey, VisibilityOff, Visibility } from '@mui/icons-material'
 import './Login.scss'
+import { loginDataValidation } from '../shared/loginDataValidation'
+import { validateEmail } from '../shared/validateEmail'
 
 export const Login: React.FC = () => {
-  const navigate = useNavigate()
+  const [state, setState] = React.useState({
+    emailError: false,
+    passwordError: false,
+    showPassword: false,
+    canLog: false,
+  })
+  const [userData, setUserData] = React.useState({
+    email: '',
+    password: '',
+  })
+
+  useEffect(() => {
+    console.log('email or password changed')
+  }, [userData.email, userData.password])
+
+  const hidePassword = (): void => {
+    setState((prevState) => ({
+      ...prevState,
+      showPassword: !prevState.showPassword,
+    }))
+  }
+  const hadnleEmailChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setUserData((prevState) => ({
+      ...prevState,
+      email: event.target.value,
+    }))
+    setState((prevState) => ({
+      ...prevState,
+      emailError: false,
+    }))
+  }
+  const hadnlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setUserData((prevState) => ({
+      ...prevState,
+      password: event.target.value,
+    }))
+    setState((prevState) => ({
+      ...prevState,
+      passwordError: false,
+    }))
+  }
+
+  const handleLogin = (): void => {
+    console.log(loginDataValidation(userData))
+  }
+
   return (
     <motion.div
       className="Login"
@@ -19,31 +75,56 @@ export const Login: React.FC = () => {
         <form autoComplete="off">
           <Box
             className="Container__email"
-            sx={{ display: 'flex', alignItems: 'flex-end' }}
+            sx={{ display: 'flex', alignItems: 'flex-end', mt: 1 }}
           >
             <MailOutline sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-            <TextField id="standard-basic" label="E-mail" variant="standard" />
+            <TextField
+              id="standard-basic"
+              label="E-mail"
+              variant="standard"
+              sx={{ width: '100%' }}
+              onChange={hadnleEmailChange}
+            />
           </Box>
           <Box
             className="Container__password"
-            sx={{ display: 'flex', alignItems: 'flex-end' }}
+            sx={{ display: 'flex', alignItems: 'flex-end', mt: 1 }}
           >
             <VpnKey sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-            <TextField id="standard-basic" label="Password" variant="standard" />
+            <FormControl sx={{ m: 1, width: '100%' }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={state.showPassword ? 'text' : 'password'}
+                onChange={hadnlePasswordChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={hidePassword}
+                    >
+                      {state.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </Box>
         </form>
         <Button
           className="Container__button"
           variant="contained"
-          color="inherit"
-          sx={{ mt: 3 }}
+          color={'info'}
+          // disabled={!state.canLog}
+          sx={{ mt: 4 }}
+          onClick={handleLogin}
         >
           Sign in
         </Button>
       </div>
       <div className="Login__Register">
         Don't have an account?{' '}
-        <Link href="/registration" color="inherit">
+        <Link href="/login" color="inherit">
           Registration
         </Link>
       </div>
