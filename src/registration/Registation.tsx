0abb8cddca0e8d1100a743050d0c3ab696304eec
Maksim Pegov <maksim.peg@gmail.com>
2022-11-22 17:53:19 +0100

    [#3] Register page implemented

--- src/registration/Registation.tsx
+++ src/registration/Registation.tsx
@@ -1,18 +1,115 @@
-import { MailOutline, VpnKey } from '@mui/icons-material'
-import { Box, FormControl, Input, InputLabel, TextField } from '@mui/material'
+import {
+  MailOutline,
+  PersonAdd,
+  Visibility,
+  VisibilityOff,
+  VpnKey,
+} from '@mui/icons-material'
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  TextField,
+  Link,
+} from '@mui/material'
 import { motion } from 'framer-motion'
 // import { AnimatePresence } from 'framer-motion/dist/framer-motion'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { onlySpaces } from '../shared/dataValodation'
+import './Registration.scss'
 
 export const Registation: React.FC = () => {
   const navigate = useNavigate()
+  const [state, setState] = React.useState({
+    emailError: false,
+    passwordError1: false,
+    passwordError2: false,
+    showPassword1: false,
+    showPassword2: false,
+    passwordConfirm: false,
+    canLog: false,
+  })
+
+  const [userData, setUserData] = React.useState({
+    email: '',
+    password: '',
+  })
+  const [password2, setPassword2] = React.useState('')
+
+  useEffect(() => {
+    if (
+      !onlySpaces(userData.email) &&
+      userData.email.length > 5 &&
+      userData.email.includes('@') &&
+      !onlySpaces(userData.password) &&
+      userData.password.length > 5 &&
+      !onlySpaces(password2) &&
+      password2.length > 5
+    ) {
+      setState((old) => ({ ...old, canLog: true }))
+    } else {
+      setState((old) => ({ ...old, canLog: false }))
+    }
+  }, [userData.email, userData.password, password2])
+
+  const hidePassword1 = (): void => {
+    setState((prevState) => ({
+      ...prevState,
+      showPassword1: !prevState.showPassword1,
+    }))
+  }
+  const hidePassword2 = (): void => {
+    setState((prevState) => ({
+      ...prevState,
+      showPassword2: !prevState.showPassword2,
+    }))
+  }
+  const hadnleEmailChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setUserData((prevState) => ({
+      ...prevState,
+      email: event.target.value.trim(),
+    }))
+    setState((prevState) => ({
+      ...prevState,
+      emailError: false,
+    }))
+  }
+  const hadnlePasswordChange1 = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setUserData((prevState) => ({
+      ...prevState,
+      password: event.target.value.trim(),
+    }))
+    setState((prevState) => ({
+      ...prevState,
+      passwordError1: false,
+      passwordError2: false,
+    }))
+  }
+  const hadnlePasswordChange2 = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword2(event.target.value)
+    setState((prevState) => ({
+      ...prevState,
+      passwordError2: false,
+    }))
+  }
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-      Registation
-      {/* <div className="Registration__Container">
-        <div className="Registration__Container__Header">Authorization</div>
+    <motion.div
+      className="Registration"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="Registration__Container">
+        <div className="Registration__Container__Header">
+          <PersonAdd sx={{ m: 'auto', pr: 1, fontSize: 30 }} />{' '}
+          <div className="Registration__Container__Header__text">Create your account</div>
+        </div>
         <form autoComplete="off">
           <Box
             className="Container__email"
@@ -21,11 +118,11 @@ export const Registation: React.FC = () => {
             <MailOutline sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
             <TextField
               id="standard-basic"
-              label="E-mail"
-              // error={state.emailError}
+              label="Your e-mail"
+              error={state.emailError}
               variant="standard"
               sx={{ width: '100%' }}
-              // onChange={hadnleEmailChange}
+              onChange={hadnleEmailChange}
             />
           </Box>
           <Box
@@ -36,22 +133,54 @@ export const Registation: React.FC = () => {
             <FormControl sx={{ m: 1, width: '100%' }} variant="standard">
               <InputLabel
                 htmlFor="standard-adornment-password"
-                // error={state.passwordError}
+                error={state.passwordError1}
               >
-                Password
+                Your password
               </InputLabel>
               <Input
                 id="standard-adornment-password"
-                // type={state.showPassword ? 'text' : 'password'}
-                error={state.passwordError}
-                onChange={hadnlePasswordChange}
+                type={state.showPassword1 ? 'text' : 'password'}
+                error={state.passwordError1}
+                onChange={hadnlePasswordChange1}
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
+                      tabIndex={-1}
                       aria-label="toggle password visibility"
-                      onClick={hidePassword}
+                      onClick={hidePassword1}
                     >
-                      {state.showPassword ? <Visibility /> : <VisibilityOff />}
+                      {state.showPassword1 ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </Box>
+          <Box
+            className="Container__password"
+            sx={{ display: 'flex', alignItems: 'flex-end', mt: 1 }}
+          >
+            <VpnKey sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <FormControl sx={{ m: 1, width: '100%' }} variant="standard">
+              <InputLabel
+                htmlFor="standard-adornment-password"
+                error={state.passwordError2}
+              >
+                Repeat password
+              </InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={state.showPassword2 ? 'text' : 'password'}
+                error={state.passwordError2}
+                onChange={hadnlePasswordChange2}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      tabIndex={-1}
+                      aria-label="toggle password visibility"
+                      onClick={hidePassword2}
+                    >
+                      {state.showPassword2 ? <Visibility /> : <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
                 }
@@ -65,18 +194,17 @@ export const Registation: React.FC = () => {
           color={'info'}
           disabled={!state.canLog}
           sx={{ mt: 4 }}
-          onClick={handleLogin}
+          // onClick={handleLogin}
         >
-          Sign in
+          Registrate
         </Button>
-      </div> */}
-      <button
-        onClick={() => {
-          navigate('/login')
-        }}
-      >
-        button
-      </button>
+      </div>
+      <div className="Registrration__Login">
+        If you have an accouunt{' '}
+        <Link href="/login" color="inherit" sx={{ fontWeight: 'bold' }}>
+          Sign in
+        </Link>
+      </div>
     </motion.div>
   )
 }
